7366c86ea93d406d2a3053857c114bd81f835c42
idealabs515 <idealabs515@gmail.com>
2018-09-13 18:06:25 +1000

    Unit test structure created

--- src/hotel/test/HotelTest.java
+++ src/hotel/test/HotelTest.java
@@ -1,3 +1,52 @@
 package hotel.test;
+
+import org.junit.After; 
+import org.junit.Before; 
+import org.junit.Test; 
+
 public class HotelTest {
+	@Before 
+    public void init() throws Exception {
+		
+	}
+	
+	@After 
+    public void destroy() throws Exception { 
+		
+	}
+	
+	@Test 
+    public void testBook() { 
+		
+	}
+	
+	@Test 
+    public void testCheckin() { 
+		
+	}
+	
+	@Test 
+    public void testAddServiceCharge() { 
+		
+	}
+	
+	@Test 
+    public void testCheckout() { 
+		
+	}
+	
+	@Test(expected = RuntimeException.class)
+    public void testCheckinNoBooking() { 
+		
+	}
+	
+	@Test(expected = RuntimeException.class)
+    public void testAddServiceChargeNotCheckedIn() { 
+		
+	}
+	
+	@Test 
+    public void testCheckoutNotCheckedIn() { 
+		
+	}
 }
