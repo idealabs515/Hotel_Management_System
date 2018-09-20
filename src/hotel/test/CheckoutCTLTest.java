4b67dd1f3f93ac93c82184a37f2c838f2404dd7c
Huseyin Caliskan <hcalishkan@gmail.com>
2018-09-20 12:45:22 +1000

    Integration test structure created

---
+++ src/hotel/test/CheckoutCTLTest.java
@@ -0,0 +1,30 @@
+package hotel.test;
+
+import org.junit.After; 
+import org.junit.Before; 
+import org.junit.Test; 
+
+public class CheckoutCTLTest {
+
+	
+	@Before 
+    public void setUp() throws Exception {
+		
+	}
+	
+	@After 
+    public void tearDown() throws Exception {
+
+	}
+	
+	@Test 
+    public void testCreditDetailsEntered() throws Exception { 
+
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void testCreditDetailsEnteredIncorrectState() { 
+
+	}
+	
+}
