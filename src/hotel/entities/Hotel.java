ddcc1917dae5d6455e2f97c2c9bb7118e42cdce0
idealabs515 <41322531+idealabs515@users.noreply.github.com>
2018-09-22 21:59:01 +1000

    Add comments for clarification
    Add comments for clarification

--- src/hotel/entities/Hotel.java
+++ src/hotel/entities/Hotel.java
@@ -88,12 +88,16 @@ public class Hotel {
 	public long book(Room room, Guest guest, 
 			Date arrivalDate, int stayLength, int occupantNumber,
 			CreditCard creditCard) {
-		
+		//check if the room available before booking
 		if (room.isAvailable(arrivalDate, stayLength)) {
 			Booking booking = room.book(guest, arrivalDate, stayLength, occupantNumber, creditCard);
+			//handle the case if the method returns null
 			if (booking != null) {
+				//get the confirmation number from booking
 				long confirmationNumber = booking.getConfirmationNumber();
+				//add booking to bookingsByConfirmationNumber map with confirmationNumber key
 				bookingsByConfirmationNumber.put(confirmationNumber, booking);
+				//return the confirmationNumber
 				return confirmationNumber; 
 			} else 
 				return 0L;
@@ -104,38 +108,44 @@ public class Hotel {
 
 	
 	public void checkin(long confirmationNumber) {
+		//get booking from bookingsByConfirmationNumber map with confirmationNumber key
 		Booking booking = bookingsByConfirmationNumber.get(confirmationNumber);
 		
+		//if there is no booking then throw exception
 		if (booking == null) {
 			throw new RuntimeException("Booking could not be found");
 		}
-		
+		//get roomId from booking
 		int roomId = booking.getRoomId();
+		//checkin the booking
 		booking.checkIn();
-		
+		//add booking to activeBookingsByRoomId map with roomId key
 		activeBookingsByRoomId.put(roomId, booking);
 	}
 
 
-	public void addServiceCharge(int roomId, ServiceType serviceType, double cost) {		
+	public void addServiceCharge(int roomId, ServiceType serviceType, double cost) {	
+		//get booking from bookingsByConfirmationNumber map with confirmationNumber key
 		Booking booking = activeBookingsByRoomId.get(roomId);
-		
+		//if there is no booking then throw exception
 		if (booking == null) {
 			throw new RuntimeException("No active booking for room");
 		}
+		//add service change to found booking
 		booking.addServiceCharge(serviceType, cost);
 	}
 
 	
 	public void checkout(int roomId) {
+		//get booking from bookingsByConfirmationNumber map with confirmationNumber key
 		Booking booking = activeBookingsByRoomId.get(roomId);
-		
+		//if there is no booking then throw exception
 		if (booking == null) {
 			throw new RuntimeException("No active booking for room");
 		}
-		
+		//checkout for found booking
 		booking.checkOut();
-		
+		//remove booking from activeBookingsByRoomId map
 		activeBookingsByRoomId.remove(roomId);
 	}
 
