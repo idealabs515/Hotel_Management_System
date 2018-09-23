270b8ceca69cb99b663fab4be8be4ad4fd402a04
idealabs515 <41322531+idealabs515@users.noreply.github.com>
2018-09-23 14:28:05 +1000

    Final Commit

--- src/hotel/entities/BookingCheckInTestCase.java
+++ src/hotel/entities/BookingCheckInTestCase.java
@@ -1,25 +1,38 @@
-
 package hotel.entities;
+/**
+ * The following JUnit testing is used to test a specific method which is independent and unit tested.
+ * @author  ChittyVaishnav
+ * @studentID 11639078
+ */
 
 import static org.junit.Assert.*;
 
 import java.util.Date;
 
+
 import org.junit.Test;
 
 import hotel.credit.CreditCard;
 import hotel.credit.CreditCardType;
-
+//The class tests check-in functionality of booking class
 public class BookingCheckInTestCase {
+
 
 
 	@Test
 	public void test() {
-	
-Room r1 = new Room(101,RoomType.SINGLE);
-		Booking booking = new Booking(new Guest("Vaishnav Reddy","9 Gooble Street",789328990), r1, new Date(), 2, 1, new CreditCard(CreditCardType.VISA,10001,543));
-	booking.checkIn();
-	assertEquals(true,booking.isCheckedIn());
+		//Creating all the mock objects needed for testing
+		Guest newGuest = new Guest("Vaishnav Reddy","9 Gooble Street",789328990);
+		Room newRoom = new Room(101,RoomType.SINGLE);
+		//Creates a current date
+		Date currentDate = new Date();
+		CreditCard cardCredentials = new CreditCard(CreditCardType.VISA,10001,543);
+		Booking newBooking = new Booking(newGuest, newRoom, currentDate, 1, 1, cardCredentials);
+//Calling the method that need to be tested
+		newBooking.checkIn();
+		//Checking the state after calling the method
+	assertEquals(true,newBooking.isCheckedIn());
 	}
 
 }
+
